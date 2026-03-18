a04c57f5889824073f81b5dec18b25095842d16a
soongu <naong526@naver.com>
2026-03-18 09:41:13 +0900

    feat: Add environment variable support and configuration files

--- src/main/java/com/example/paymentsystem/PaymentSystemApplication.java
+++ src/main/java/com/example/paymentsystem/PaymentSystemApplication.java
@@ -1,5 +1,6 @@
 package com.example.paymentsystem;
 
+import com.example.paymentsystem.common.config.DotenvInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,7 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class PaymentSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentSystemApplication.class, args);
+     SpringApplication app = new SpringApplication(PaymentSystemApplication.class);
+        app.addInitializers(new DotenvInitializer());
+        app.run(args);
     }
 
 }
