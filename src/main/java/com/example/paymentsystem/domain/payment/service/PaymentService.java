e22afd2965584e9802b74e4656e4a1178293ae9e
Taeguni06 <aqwsde0412@gmail.com>
2026-03-19 20:38:28 +0900

    feat: 결제 시도 기능 추가 및 결제 ID 랜덤 생성 클래스 추가, PaymentStatus 리팩터링

--- src/main/java/com/example/paymentsystem/domain/payment/service/PaymentService.java
+++ src/main/java/com/example/paymentsystem/domain/payment/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.example.paymentsystem.domain.payment.service;
 
+import com.example.paymentsystem.domain.order.entity.Order;
+import com.example.paymentsystem.domain.order.repository.OrderRepository;
 import com.example.paymentsystem.domain.payment.dto.PaymentTryRequest;
 import com.example.paymentsystem.domain.payment.dto.PaymentTryResponse;
 import com.example.paymentsystem.domain.payment.dto.WebhookRequestDto;
@@ -7,7 +9,7 @@ import com.example.paymentsystem.domain.payment.entity.Payment;
 import com.example.paymentsystem.domain.payment.entity.Webhook;
 import com.example.paymentsystem.domain.payment.repository.PaymentRepository;
 import com.example.paymentsystem.domain.payment.repository.WebhookRepository;
-import com.example.paymentsystem.domain.payment.status.PaymentStatus;
+import com.example.paymentsystem.domain.payment.entity.PaymentStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,14 +19,32 @@ import org.springframework.transaction.annotation.Transactional;
 public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final WebhookRepository webhookRepository;
+    private final OrderRepository orderRepository;
+    private final PaymentIdGenerator paymentIdGenerator;
 
     public PaymentTryResponse tryPayment(Long orderId, PaymentTryRequest request) {
 
-//        Order order = orderRepository.findById(orderId);
+        //1. 존재 하는 주문인지
+
+        Order order = orderRepository.findById(orderId).orElseThrow(
+                () -> new IllegalStateException("주문을 찾을 수 없습니다.")
+        );
+
+        //2. 주문 금액이 결제 금액과 동일한지
+
+        if (order.getTotalAmount() != request.payment_price().intValue()) {
+            throw new IllegalStateException("주문 금액이 일치하지 않습니다.");
+        }
+
+        //3. PortOne에 보낼 payments_id 생성
+
+        String generatedPaymentId = paymentIdGenerator.generate(String.valueOf(order.getUserId()));
+
+        //4. 결제 테이블에 저장 (주문 id, payments_id, 결제 상태(대기 기본값), 결제 금액)
 
         return new PaymentTryResponse(paymentRepository.save(new Payment(
-                orderId,
-                request.payments_id(),
+                order,
+                generatedPaymentId,
                 PaymentStatus.WAIT,
                 request.payment_price())));
     }
