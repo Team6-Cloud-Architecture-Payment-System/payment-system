4a53f3284a01e5d48f6febae40502981e470f2d7
fryzke <fryekdy@gmail.com>
2026-03-19 16:17:19 +0900

    fix: 테이블명 복수형으로 수정

--- src/main/java/com/example/paymentsystem/domain/auth/entity/User.java
+++ src/main/java/com/example/paymentsystem/domain/auth/entity/User.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name="user")
+@Table(name="users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
     @Id
