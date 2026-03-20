a9d583d495ae6388dbc06c89731ffb4443d65bbe
“grown-tree” <tjsruddks12@naver.com>
2026-03-20 17:23:44 +0900

    fix : stash commit

---
+++ src/main/java/com/example/paymentsystem/domain/payment/dto/PortOneResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.paymentsystem.domain.payment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PortOneResponseDto {
+
+    //가져와야할 항목 : 결제 상태, 결제된 총금액(amount>total)
+}
