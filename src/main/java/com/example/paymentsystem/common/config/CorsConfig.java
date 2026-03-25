9c8c4e8d8568b0bce346be71ea7d4d6b01e0ee28
Ho-jin98 <hohoj98@naver.com>
2026-03-25 17:03:10 +0900

    feat: 배포 설정파일 추가 (#110)
    * feat: 배포 설정파일 추가
    
    * feat: SecurityConfig 수정

---
+++ src/main/java/com/example/paymentsystem/common/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.example.paymentsystem.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration  = new CorsConfiguration();
+        configuration .setAllowedOriginPatterns(
+                Arrays.asList(
+                        "http://localhost:8080",
+                        "http://3.36.86.233:8080",
+                        "https://dogpedia.store",
+                        "https://www.dogpedia.store"
+                )
+        );
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
